f4449f498082ff8f4e9efd687c038d64bc6056ba
privateaccount849 <80937560+privateaccount849@users.noreply.github.com>
2022-04-13 02:02:08 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,7 +1,8 @@
 <?php
 $hostname = "localhost";
 $username = "id18774357_root";
-$password = "{c?WTnHBT]ktE>$9";
+$password = "PSOFpl(SqyS4c~N+";
 $db = "id18774357_vietvivu";
 
 $con = mysqli_connect($hostname, $username, $password,$db);
+is
