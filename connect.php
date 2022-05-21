edb7f16af35764f46a20905fd55c72c1f0e951e3
NgoThanhLong <80937560+privateaccount849@users.noreply.github.com>
2022-05-21 09:37:09 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
-$hostname = "localhost";
-$username = "root";
-$password = "";
+$hostname = "us-cdbr-east-05.cleardb.net";
+$username = "b2fa05b1ce3fc7";
+$password = "f3bcec37";
 $db = "vietvivu";
 
 $con = mysqli_connect($hostname, $username, $password,$db);
